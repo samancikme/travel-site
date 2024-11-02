123b7814553afb3c027ebaa5c5e4401e4e1f02ba
Ikram <ikramtlepov13@gmail.com>
2024-11-03 01:19:09 +0500

    travel-tour

--- src/components/pageComp/DestCard.jsx
+++ src/components/pageComp/DestCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DestCard = ({item}) => {
   return (
-    <div className='p-[10px] rounded-md shadow-sm border-[1px] h-full flex flex-col justify-between gap-1 '>
+    <div className='p-[10px] rounded-md shadow-md border-[1px] h-full flex flex-col justify-between gap-1 '>
         <div>
           <img className='h-[200px] object-cover rounded-md shadow-sm ' src={item.image} alt="" />
         </div>
